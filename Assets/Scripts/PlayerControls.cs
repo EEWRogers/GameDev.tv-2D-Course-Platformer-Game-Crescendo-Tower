5bcbbc96a5e6db31e11d6273ebf20d89d76e18ea
Elliott Rogers <elliott.e.w.rogers@gmail.com>
2024-02-07 16:49:10 +0000

    Added jump action Worked on controls script

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,18 +5,46 @@ using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+    Rigidbody2D playerRigidbody;
+    Vector2 movementVector;
+
     PlayerInput playerInput;
     InputAction moveAction;
+    InputAction jumpAction;
 
     void Awake() 
     {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions["Move"];
+        jumpAction = playerInput.actions["Jump"];
+    }
+
+    void OnEnable() 
+    {
+        jumpAction.performed += Jump;
+    }
+
+    void OnDisable() 
+    {
+        jumpAction.performed -= Jump;
     }
 
     void Update() 
     {
-        Debug.Log(moveAction.ReadValue<float>());
+        Move();
+    }
+
+    void Move()
+    {
+        movementVector = moveAction.ReadValue<Vector2>();
+        playerRigidbody.velocity = movementVector;
+    }
+
+    void Jump(InputAction.CallbackContext context)
+    {
+        Debug.Log("You jumped!");
     }
 
 }
